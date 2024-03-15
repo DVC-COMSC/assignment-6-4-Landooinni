10e8a4ad35dbf3172999731c18911983319e59f0
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-15 13:29:12 -0700

    awsed

--- main.hpp
+++ main.hpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 void swapValues(int &n1, int &n2){int j=n1;n1=n2;n2=j;}
-void swapValues(int &n1, int &n2, int &n3){int j=n1;n1=n2;n2=n3;n3=j;}
-void swapValues(int &n1, int &n2, int &n3, int &n4){int j=n1;n1=n2;n2=n3;n3=n4;n4=j;}
+void swapValues(int &n1, int &n2, int &n3){int j=n2;n2=n3;n3=j;}
+void swapValues(int &n1, int &n2, int &n3, int &n4){int j=n3;n3=n4;n4=j;}
